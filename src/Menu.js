ee4ad8682eff4d1f3b656c1ba3d020aa75a88173
rst0070 <52444137+rst0070@users.noreply.github.com>
2021-02-01 19:51:06 +0900

    .

--- src/Menu.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import "./menu.css";
-export default function Menu() {
-  return <div id="menu_pannel">ddd</div>;
-}
--- src/index.js
+++ src/index.js
@@ -2,18 +2,8 @@ import ReactDOM from "react-dom";
 
 import List from "./List";
 import Navbar from "./Navbar";
-import Menu from "./Menu";
-/*
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);*/
+
 let navbar = Navbar();
-let menu = Menu();
 let list = List("study", 0);
 ReactDOM.render(navbar, document.getElementById("navbar_container"));
-ReactDOM.render(menu, document.getElementById("menu_container"));
 ReactDOM.render(list, document.getElementById("list_container"));
